47f7b19600df88f1006c6a0699a769ebc72baca9
foree <daniel2008lee@163.com>
2021-04-11 18:23:58 +0800

    增加 ok3 测试入口

--- app/src/main/java/org/foree/ok3test/MainActivity.java
+++ app/src/main/java/org/foree/ok3test/MainActivity.java
@@ -3,12 +3,47 @@ package org.foree.ok3test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private OkHttpClient okHttpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        okHttpClient = new OkHttpClient();
+
+        Button testOk = findViewById(R.id.testOk);
+
+        testOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Request request = new Request.Builder().url("https://www.wanandroid.com/article/list/0/json").build();
+                okHttpClient.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Log.d(TAG, "error: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        Log.d(TAG, "response: " + response.body() != null ? response.body().string(): "null");
+                    }
+                });
+            }
+        });
     }
 }
